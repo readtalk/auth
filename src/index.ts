d92598e3dcef5f1cd17e8089330ab60c0730bfda
READTalk <redtalk.app@hotmail.com>
2026-05-30 20:11:39 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,75 +6,67 @@ import { createSubjects } from "@openauthjs/openauth/subject";
 import { object, string } from "valibot";
 
 const subjects = createSubjects({
-  user: object({
-    id: string(),
-  }),
+	user: object({
+		id: string(),
+	}),
 });
 
 export default {
-  fetch(request: Request, env: Env, ctx: ExecutionContext) {
-    return issuer({
-      storage: CloudflareStorage({
-        namespace: env.AUTH_STORAGE,
-      }),
+	fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		return issuer({
+			storage: CloudflareStorage({
+				namespace: env.AUTH_STORAGE,
+			}),
 
-      subjects,
+			subjects,
 
-      providers: {
-        password: PasswordProvider(
-          PasswordUI({
-            sendCode: async (email, code) => {
-              console.log(`Sending code ${code} to ${email}`);
-            },
+			providers: {
+				password: PasswordProvider(
+					PasswordUI({
+						sendCode: async (email, code) => {
+							console.log(`Sending code ${code} to ${email}`);
+						},
+						copy: {
+							input_code: "Code",
+						},
+					}),
+				),
+			},
 
-            copy: {
-              input_code: "Verification Code",
-            },
-          }),
-        ),
-      },
+			theme: {
+				title: "READTalk Auth",
+				primary: "#ff0000",
+				favicon: "https://readtalk.pages.dev/vite.svg",
+				logo: {
+					dark: "https://readtalk.pages.dev/vite.svg",
+					light: "https://readtalk.pages.dev/vite.svg",
+				},
+			},
 
-      theme: {
-        title: "READTalk Authentication",
-        primary: "#ff0000",
-        favicon: "https://readtalk.pages.dev/vite.svg",
-        logo: {
-          dark: "https://readtalk.pages.dev/vite.svg",
-          light: "https://readtalk.pages.dev/vite.svg",
-        },
-      },
-
-      success: async (ctx, value) => {
-        const userID = await getOrCreateUser(env, value.email);
-
-        return ctx.subject("user", {
-          id: userID,
-        });
-      },
-    }).fetch(request, env, ctx);
-  },
+			success: async (ctx, value) => {
+				return ctx.subject("user", {
+					id: await getOrCreateUser(env, value.email),
+				});
+			},
+		}).fetch(request, env, ctx);
+	},
 } satisfies ExportedHandler<Env>;
 
-async function getOrCreateUser(
-  env: Env,
-  email: string,
-): Promise<string> {
-  const result = await env.AUTH_DB.prepare(`
-      INSERT INTO user (email)
-      VALUES (?)
-      ON CONFLICT (email) DO UPDATE SET email = email
-      RETURNING id;
-    `)
-    .bind(email)
-    .first<{ id: string }>();
+async function getOrCreateUser(env: Env, email: string): Promise<string> {
+	const result = await env.AUTH_DB.prepare(
+		`
+		INSERT INTO user (email)
+		VALUES (?)
+		ON CONFLICT (email) DO UPDATE SET email = email
+		RETURNING id;
+		`
+	)
+		.bind(email)
+		.first<{ id: string }>();
 
-  if (!result) {
-    throw new Error(`Unable to process user: ${email}`);
-  }
+	if (!result) {
+		throw new Error("User error");
+	}
 
-  console.log(
-    `Found or created user ${result.id} with email ${email}`,
-  );
-
-  return result.id;
+	return result.id;
 }
