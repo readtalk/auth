0aadd3aa084eaa3cad375728e1b1f2ebc7bd0cdf
READTalk <redtalk.app@hotmail.com>
2026-05-31 06:18:57 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -49,13 +49,13 @@ export default {
 				),
 			},
 			theme: {
-				title: "myAuth",
-				primary: "#0051c3",
-				favicon: "https://workers.cloudflare.com//favicon.ico",
+				title: "Authentication",
+				primary: "#FF0000",
+				favicon: "https://readtalk.pages.dev/vite.svg",
 				logo: {
-					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+					dark: "https://readtalk.pages.dev/vite.svg",
 					light:
-						"https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+						"https://readtalk.pages.dev/vite.svg",
 				},
 			},
 			success: async (ctx, value) => {
