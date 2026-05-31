5eb82ec6bfae944205e1a38dd324f60a62e24c27
READTalk <redtalk.app@hotmail.com>
2026-05-31 10:40:44 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -119,11 +119,11 @@ export default {
 			},
 			theme: {
 				title: "READTalk - Authentication",
-				primary: "#000000",
+				primary: "#FF0000",
 				favicon: "https://readtalk.pages.dev/favicon.ico",
 				logo: {
-					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
-					light: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+					dark: "https://readtalk.pages.dev/vite.svg",
+					light: "https://readtalk.pages.dev/vite.svg",
 				},
 			},
 			success: async (ctx, value) => {
