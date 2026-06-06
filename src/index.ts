fdeae8a7364443924a55fd30cb79e64b1e34dd85
READTalk <redtalk.app@hotmail.com>
2026-06-06 20:42:48 +0700

    Update favicon URL in theme configuration

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ export default {
       theme: {
         title: "Authentication",
         primary: "#FF0000",
-        favicon: "https://auth.readtalk.workers.dev//favicon.ico",
+        favicon: "https://auth.readtalk.workers.dev/public/favicon.ico",
         logo: {
           dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
           light: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
